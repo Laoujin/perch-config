96bd9427b1a56eafc861f1fe6cf53a2db014a09f
Laoujin <woutervs@hotmail.com>
2026-04-06 15:56:26 +0200

    improve terminal.ps1

--- powershell/scripts/terminal.ps1
+++ powershell/scripts/terminal.ps1
@@ -1,9 +1,46 @@
+$script:CdAliasesPath = "$PSScriptRoot\..\cd-aliases.ini"
+
+function Get-CdAliases {
+    $aliases = @{}
+    if (Test-Path $script:CdAliasesPath) {
+        Get-Content $script:CdAliasesPath | ForEach-Object {
+            if ($_ -match '^([^#\[=]+)=(.+)$') {
+                $aliases[$matches[1].Trim()] = $matches[2].Trim()
+            }
+        }
+    }
+    $aliases
+}
+
 function Start-DevTerminal {
     param(
-        [string]$Dir = "C:\Users\woute\Dropbox\Personal\Programming\UnixCode"
+        [string]$Project,
+        [string]$Dir
     )
+
+    if ($Project) {
+        $aliases = Get-CdAliases
+        if ($aliases.ContainsKey($Project)) {
+            $Dir = $aliases[$Project]
+        } else {
+            Write-Error "Unknown project: $Project"
+            return
+        }
+    }
+
+    if (-not $Dir) {
+        $Dir = "C:\Users\woute\Dropbox\Personal\Programming\UnixCode"
+    }
+
     $WslDir = $Dir -replace '^C:', '/mnt/c' -replace '\\', '/'
     wt -p "PowerShell" -d $Dir `; split-pane -V -p "Ubuntu" -d $WslDir
+}
+
+Register-ArgumentCompleter -CommandName Start-DevTerminal -ParameterName Project -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    (Get-CdAliases).Keys | Where-Object { $_ -like "$wordToComplete*" } | Sort-Object | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
 }
 
 Set-Alias dev Start-DevTerminal
